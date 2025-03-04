2f16e8dcfe535a5f8c2a0bd3a19aa3ecfbb592b6
Matt K <matthew.b.kurtti@gmail.com>
2025-03-04 20:42:05 +0900

    feat: add wizard pic and coming soon text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import wizard from "./assets/wizard.jpg";
 
 function App() {
   return (
     <>
       <h1>Welcome to Arcane Kitchen!</h1>
+      <img src={wizard} alt="wizard" />
+      <h1>More Coming Soon!</h1>
     </>
   );
 }
