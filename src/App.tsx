5013170ab6faf9b8a128a030296f60f7088f8e37
Matt K <matthew.b.kurtti@gmail.com>
2025-03-06 14:40:06 +0900

    install: tailwind, react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import "./App.css";
 import wizard from "./assets/wizard.jpg";
-
-// this is a test 2
 
 function App() {
   return (
     <>
-      <h1>Welcome to Arcane Kitchen!</h1>
-      <img src={wizard} alt="wizard" />
+      <h1 className="text-3x1 font-bold underline">
+        Welcome to Arcane Kitchen!
+      </h1>
+      <img className="w-1/2" src={wizard} alt="wizard" />
       <h1>More Coming Soon!</h1>
     </>
   );
