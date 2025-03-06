182d9c1c757b9b4c63a12b1f608f6bb45be4e0b6
JesusRivera00 <jesus.rivera.gastelum@gmail.com>
2025-03-06 21:00:17 +0900

    fix:component-name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import SignUp from './Register';
+import SignUp from './SignUp';
 
 function App() {
   return (
