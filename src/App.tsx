fede7ff547fe7a531a3b94cd254a6bc7d787568b
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-07 10:36:46 +0900

    feat: add validation for password length and confirmation in signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import SignUp from './SignUp';
+import Home from './components/Home';
+import SignUp from './components/Auth/SignUp';
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
+import { AuthContextProvider } from './components/Auth/AuthContext.tsx';
 import './index.css';
 import App from './App.tsx';
-import { AuthContextProvider } from './components/Auth/AuthContext.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <AuthContextProvider>
