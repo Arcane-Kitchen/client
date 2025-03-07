d7f105abe520269b27931a2b66492a2630006821
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-07 13:55:08 +0900

    feat: implement form submission and user profile creation on sign-up

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,2 +1,20 @@
 const baseUrl = import.meta.env.VITE_API_BASE_URL
 
+// Make a POST request to create a new user profile
+export const createUserProfile = async (id:string, username:string) => {
+    try {
+        const response = await fetch(`${baseUrl}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 'supabaseId': id, username })
+        })
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error creating user profile: ', error);
+        throw error;
+    }
+}
