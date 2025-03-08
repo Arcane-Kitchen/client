e728059f80d8b48d60554b23bbd687cca84d3aa2
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-08 20:45:30 +0900

    feat: implement function to fetch all recipes

--- src/components/RecipesPage.tsx
+++ src/components/RecipesPage.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import { useAuth } from "../Auth/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import paper from "../assets/old-style-paper.png";
+import { fetchAllRecipes } from "../api/recipeApi";
+
+export interface recipe {
+  id: string,
+  name: string,
+  description: string,
+  image: string,
+}
 
 const RecipesPage: React.FC = () => {
-  const { signOut } = useAuth();
-  const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      await signOut();
-      navigate("/login");
-    } catch (error) {
-      console.error("An error occurred: ", error);
+  const [recipes, setRecipes] = useState<recipe[]>([]);
+
+  useEffect(() => {
+    fetchAllRecipes()
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  useEffect(() => {
+    if (recipes.length > 0) {
+      console.log(recipes);
     }
-  };
+  },[recipes])
 
   return (
     <div className="bg-[url('./assets/background.png')] bg-cover min-h-screen flex flex-col items-center justify-center pt-16">
@@ -23,15 +31,25 @@ const RecipesPage: React.FC = () => {
       <h1 className="text-3xl font-bold underline text-white mb-8 mt-3">
         Available Recipes
       </h1>
-      <img src={paper} alt="old style paper" className="w-2/3 mb-8" />
-      <img src={paper} alt="old style paper" className="w-2/3 mb-8" />
-      <img src={paper} alt="old style paper" className="w-2/3 mb-8" />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>
+      <div className="grid grid-cols-1 w-3/4 gap-10 mb-10">
+        {recipes && recipes.length > 0 &&
+          recipes.map((recipe, index) => (
+            <div
+              key={index} 
+              className="w-full h-full bg-[url('./assets/old-style-paper.png')] bg-cover bg-center px-15 py-5 flex"
+            >
+              <div className="flex flex-col gap-3">
+                <h1>{recipe.name}</h1>
+                <p>{recipe.description}</p>
+              </div>
+              <div>
+                <img src={recipe.image} />
+              </div>
+            </div>
+          ))
+        }
+
+      </div>
     </div>
   );
 };
