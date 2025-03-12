d37f5cf11e66f572b681a294ab372e6b4203a91d
Matt K <matthew.b.kurtti@gmail.com>
2025-03-12 11:34:21 +0900

    feat: show user total exp on profile

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import RecipesPage from "./pages/RecipesPage";
 import CalendarPage from "./pages/CalendarPage";
 import ProfilePage from "./pages/ProfilePage";
 import Layout from "./pages/Layout";
-import NewRecipePage from './components/NewRecipePage';
+import NewRecipePage from "./components/NewRecipePage";
 import { useAuth } from "./Auth/AuthContext";
 import { fetchFullUserMealPlan } from "./api/mealPlanApi";
-import { fetchARecipeById } from "./api/recipeApi"
+import { fetchARecipeById } from "./api/recipeApi";
 
 export interface Meal {
-  id: string,
+  id: string;
   recipe_id: string;
   day_to_eat: string;
   chosen_meal_type: string;
@@ -27,12 +27,12 @@ export interface MealPlan {
   title: string;
   start: Date;
   end: Date;
-  imageUrl: string,
+  imageUrl: string;
   hasBeenEaten: boolean;
+  exp: number;
 }
 
 function App() {
-
   const [mealPlan, setMealPlan] = useState<MealPlan[]>([]);
   const { user, session, setIsLoading } = useAuth();
 
@@ -41,53 +41,69 @@ function App() {
       setIsLoading(true);
       const fetchUserMealData = async () => {
         try {
-          const mealPlan = await fetchFullUserMealPlan(user.id, session.access_token);
-          const mappedMealPlan = await Promise.all(mealPlan.map(async (meal: Meal) => {
-            let start = new Date(meal.day_to_eat);
-  
-            if (meal.chosen_meal_type === "breakfast") {
-              start.setHours(0, 0, 0);
-            } else if (meal.chosen_meal_type === "lunch") {
-              start.setHours(8, 0, 0);
-            } else {
-              start.setHours(16, 0, 0);
-            }
-  
-            let end = new Date(start);
-            end.setHours(start.getHours() + 7);
-  
-            let recipe = await fetchARecipeById(meal.recipe_id)
-  
-            return {
-              id: meal.id,
-              title: meal.recipe_id,
-              start,
-              end,
-              imageUrl: recipe.image,
-              hasBeenEaten: meal.has_been_eaten,
-            }
-          }))
+          const mealPlan = await fetchFullUserMealPlan(
+            user.id,
+            session.access_token
+          );
+          const mappedMealPlan = await Promise.all(
+            mealPlan.map(async (meal: Meal) => {
+              const start = new Date(meal.day_to_eat);
+
+              if (meal.chosen_meal_type === "breakfast") {
+                start.setHours(0, 0, 0);
+              } else if (meal.chosen_meal_type === "lunch") {
+                start.setHours(8, 0, 0);
+              } else {
+                start.setHours(16, 0, 0);
+              }
+
+              const end = new Date(start);
+              end.setHours(start.getHours() + 7);
+
+              const recipe = await fetchARecipeById(meal.recipe_id);
+
+              return {
+                id: meal.id,
+                title: meal.recipe_id,
+                start,
+                end,
+                imageUrl: recipe.image,
+                hasBeenEaten: meal.has_been_eaten,
+                exp: meal.exp,
+              };
+            })
+          );
           setMealPlan(mappedMealPlan);
-        } catch (error:any) {
+        } catch (error: any) {
           console.error(error);
         } finally {
           setIsLoading(false);
         }
-      }
+      };
       fetchUserMealData();
     }
-  }, [session])
+  }, [session]);
 
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} /> {/* Default route */}
-        <Route path="profile" element={<ProfilePage />} />
+        <Route path="profile" element={<ProfilePage mealPlan={mealPlan} />} />
         <Route path="/new-recipe" element={<NewRecipePage />} />
-        <Route path="recipes" element={<RecipesPage mealPlan={mealPlan} setMealPlan={setMealPlan} />} />
-        <Route path="calendar" element={<CalendarPage mealPlan={mealPlan} setMealPlan={setMealPlan} />} />
+        <Route
+          path="recipes"
+          element={
+            <RecipesPage mealPlan={mealPlan} setMealPlan={setMealPlan} />
+          }
+        />
+        <Route
+          path="calendar"
+          element={
+            <CalendarPage mealPlan={mealPlan} setMealPlan={setMealPlan} />
+          }
+        />
       </Route>
       <Route path="*" element={<Home />} />
     </Routes>
