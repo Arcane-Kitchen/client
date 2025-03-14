49de7df6dc2f71e13b77c13bd5821925a60820e6
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-14 17:17:38 +0900

    feat: implement desktop view for calendar page

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -47,14 +47,14 @@ const CalendarPage: React.FC<CalendarPageProps> = ({ mealPlan, setMealPlan }) =>
 
   return (
     <div className="h-fit flex flex-col items-center justify-center p-10 ">
-      <div className="flex-1 bg-[url('/paper-box.jpg')] bg-cover bg-center w-full p-5">
+      <div className="flex-1 bg-[url('/paper-box.jpg')] bg-cover bg-center w-full p-5 lg:p-15">
         {isLoading ? (
           <PacmanLoader />
         ) : (
           <div className="flex flex-col">
-
             <h3 className="text-2xl mb-2">{`${currentStartOfWeek.format("MMMM DD")} - ${currentStartOfWeek.clone().add(6, "days").format("DD")}`}</h3>
 
+            {/* Buttons */}
             <div className="flex items-center border-1 border-gray-300 divide-x-1 divide-gray-300 mb-5 w-3/4 rounded-md">
               <button 
                 className={`flex-1 py-1 rounded-tl-md rounded-bl-md ${currentStartOfWeek.isSame(moment().startOf("week")) ? "bg-[#19243e] text-[#ebd6aa]" : "hover:scale-105 hover:cursor-pointer hover:bg-[#19243e] hover:text-[#ebd6aa] hover:shadow-lg"}`}
@@ -76,44 +76,49 @@ const CalendarPage: React.FC<CalendarPageProps> = ({ mealPlan, setMealPlan }) =>
               </button>
             </div>
 
-            <div className="flex w-full">
-              <div className="w-1/4 bg-[#19243e]"></div>
-              {["Breakfast", "Lunch", "Dinner"].map((type) => (
-                <div key={type} className="w-1/4 bg-[#19243e] text-[#ebd6aa] text-center">{type}</div>
-              ))}
-            </div>
-            
-            {/* Rows for each day of the week */}
-            {daysOfWeek.map((day, index) => {
-              return (
-                <div className="flex w-full border-b-1 border-l-1 border-gray-300">
-                  {/* Day header */}
-                  <div key={day} className="w-1/4 flex items-center p-2 border-r-1 border-gray-300">
-                    <p className="flex-1">{day}</p>
-                  </div>
+            {/* Calendar */}
+            <div className="flex flex-col lg:flex-row lg:border-l-1 lg:border-b-1 lg:border-gray-300">
+              {/* Meal type header */}
+              <div className="flex w-full lg:flex-col lg:w-3xl divide-y-1 divide-gray-300">
+                <div className="w-1/4 bg-[#19243e] lg:w-full lg:h-1/13"></div>
+                {["Breakfast", "Lunch", "Dinner"].map((type) => (
+                  <div key={type} className="w-1/4 bg-[#19243e] text-[#ebd6aa] text-center lg:w-full lg:flex-1 lg:text-left lg:px-2 lg:bg-transparent lg:text-[#19243e] lg:text-lg lg:flex lg:items-center">{type}</div>
+                ))}
+              </div>
+              
+              {/* Day header */}
+              {daysOfWeek.map((day, index) => {
+                return (
+                  <div key={day} className="flex w-full lg:flex-col border-l-1 border-gray-300 divide-y-1 divide-gray-300">
+                    {/* Day header */}
+                    <div className="w-1/4 flex items-center p-2 border-r-1 border-gray-300 lg:w-full lg:text-center lg:bg-[#19243e] lg:text-[#ebd6aa]">
+                      <p className="flex-1">{day}</p>
+                    </div>
 
-                  {["breakfast", "lunch", "dinner"].map((type) => {
-                    const weeklyMeals = mealPlan.filter((meal) => {
-                      return meal.date === currentStartOfWeek.clone().add(index, "days").format("M/DD/YYYY") && meal.mealType.toLowerCase() === type;
+                    {["breakfast", "lunch", "dinner"].map((type) => {
+                      const weeklyMeals = mealPlan.filter((meal) => {
+                        return meal.date === currentStartOfWeek.clone().add(index, "days").format("M/DD/YYYY") && meal.mealType.toLowerCase() === type;
+                      })
+                      
+                      if (weeklyMeals.length === 0) {
+                        return (
+                          <div key={`${day}-${type}`} className="border-r-1 border-gray-300 w-1/4 h-20 lg:w-full lg:h-40"></div>
+                        );
+                      }
+
+                      return weeklyMeals.map((meal) => (
+                        <div key={`${day}-${type}-${meal.id}`} className="border-r-1 border-gray-300 w-1/4 h-20 p-1 relative lg:w-full lg:h-40">
+                          <div className="w-full h-full text-center bg-cover bg-center hover:cursor-pointer" style={{ backgroundImage: `url(${meal.imageUrl})` }}></div>
+                          {meal.hasBeenEaten && <FaCheckCircle color="green" className="absolute bottom-0.5 right-0.5"/>}
+                        </div>
+                      ))
                     })
-                    
-                    if (weeklyMeals.length === 0) {
-                      return (
-                        <div key={`${day}-${type}`} className="border-r-1 border-gray-300 w-1/4 h-20"></div>
-                      );
                     }
+                  </div>
+                );
+              })}
+            </div>
 
-                    return weeklyMeals.map((meal) => (
-                      <div className="border-r-1 border-gray-300 w-1/4 h-20 p-1 relative">
-                        <div key={meal.id} className="w-full  h-full text-center bg-cover bg-center hover:cursor-pointer" style={{ backgroundImage: `url(${meal.imageUrl})` }}></div>
-                        {meal.hasBeenEaten && <FaCheckCircle color="green" className="absolute bottom-0.5 right-0.5"/>}
-                      </div>
-                    ))
-                  })
-                  }
-                </div>
-              );
-            })}
           </div>
         )}
       </div>
