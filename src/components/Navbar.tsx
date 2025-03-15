2bc0ddc0d8a758909bb6db372bda2367bddea174
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-15 21:33:14 +0900

    feat: refactor navbar UI for desktop view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,17 +23,17 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 h-16 w-full z-10">
+    <div className="fixed top-0 left-0 h-16 w-full z-10 lg:h-24">
       <div className="flex items-center justify-between h-full bg-[#19243e]">
-      <img src="/navbar.svg" className="h-16 transform scale-y-150 relative top-0 left-0"/>
-        <div className="absolute top-0 left-0 z-20 h-full w-full flex items-center justify-between px-10">
-          <div className="text-[#ebd6aa] text-2xl font-bold">Arcane Kitchen</div>
+      <img src="/navbar.svg" className="h-16 transform scale-y-150 relative top-0 left-0 lg:w-full lg:h-full lg:scale-y-100 lg:scale-x-145"/>
+        <div className="absolute top-0 left-0 z-20 h-full w-full flex items-center justify-between px-10 lg:px-25">
+          <div className="text-[#ebd6aa] text-2xl font-bold lg:text-4xl">Arcane Kitchen</div>
           <button
             onClick={toggleNavbar}
             className="text-[#ebd6aa] focus:outline-none"
           >
             <svg
-              className="w-8 h-8 cursor-pointer"
+              className="w-8 h-8 cursor-pointer lg:w-12 lg:h-12"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -51,32 +51,32 @@ const Navbar: React.FC = () => {
       </div>
       {isOpen && (
         <div
-          className="absolute top-16 right-0 w-full bg-[#19243e] text-[#ebd6aa] text-lg opacity-95 p-4" 
+          className="absolute top-16 right-0 w-full bg-[#19243e] text-[#ebd6aa] text-lg opacity-95 p-4 lg:top-24" 
         >
             <nav className="w-full">
               <ul className="w-full flex flex-col items-center text-center">
-                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white  ${session ? 'hidden' : 'block'}`} onClick={toggleNavbar}>
+                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'hidden' : 'block'}`} onClick={toggleNavbar}>
                   <Link to='/'>Home</Link>
                 </li>
-                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white ${session ? 'block' : 'hidden'}`} onClick={toggleNavbar}>
+                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'block' : 'hidden'}`} onClick={toggleNavbar}>
                   <Link to='/profile'>Profile</Link>
                 </li>
-                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white  ${session ? 'block' : 'hidden'}`} onClick={toggleNavbar}>
+                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'block' : 'hidden'}`} onClick={toggleNavbar}>
                   <Link to='/meal-plan'>Meal Plan</Link>
                 </li>
-                <li className={"w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white"} onClick={toggleNavbar}>
+                <li className={"w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl"} onClick={toggleNavbar}>
                   <Link to='/recipes'>Recipes</Link>
                 </li>
                 <li 
-                  className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white  ${session ? 'block' : 'hidden'}` } 
+                  className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'block' : 'hidden'}` } 
                   onClick={handleLogout}
                 >
                   Logout
                 </li>
-                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white  ${session ? 'hidden' : 'block'}` } onClick={toggleNavbar}>
+                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'hidden' : 'block'}` } onClick={toggleNavbar}>
                   <Link to='/signup'>Sign Up</Link>
                 </li>
-                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white  ${session ? 'hidden' : 'block'}`} onClick={toggleNavbar}>
+                <li className={`w-4/5 py-2 px-4 rounded-lg cursor-pointer hover:bg-[#264D77] hover:text-white lg:text-2xl ${session ? 'hidden' : 'block'}`} onClick={toggleNavbar}>
                   <Link to='/login'>Login</Link>
                 </li>
               </ul>
