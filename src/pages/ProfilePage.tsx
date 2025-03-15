e337f58124dc68143fd03b2131255461a56c4376
Matt K <matthew.b.kurtti@gmail.com>
2025-03-15 12:00:50 +0900

    unfinished: working with grabbing past day meals

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -22,13 +22,15 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ mealPlan }) => {
   const checkDailyCalories = async () => {
     const lastLogin = user?.updated_at;
     const currentTime = new Date(lastLogin);
+    // const currentTimeToIso = currentTime.toISOString();
     const startOfDay = new Date(lastLogin);
     startOfDay.setHours(0, 0, 0, 0);
+    const startOfDayToIso = startOfDay.toISOString();
 
     const recipes = await fetchUserPastDayRecipes(
       user?.id,
-      startOfDay,
-      currentTime
+      startOfDayToIso,
+      user?.updated_at
     );
     console.log(recipes);
   };
