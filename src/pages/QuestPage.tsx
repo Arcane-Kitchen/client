3dfc1558995ac0167af6aa2d2a091d5b25313bdf
Matt K <matthew.b.kurtti@gmail.com>
2025-03-17 16:04:15 +0900

    feats: user can fight. If they win, shows next enemy. Else, tells them what stats need to go up

--- src/pages/QuestPage.tsx
+++ src/pages/QuestPage.tsx
@@ -2,28 +2,66 @@ import React, { useEffect, useState } from "react";
 // import neutralDragon from "../assets/neutral.png";
 // import sadDragon from "../assets/sad.png";
 import { useAuth } from "../Auth/AuthContext";
-import { useNavigate } from "react-router-dom";
 import { fetchEnemyById } from "../api/enemyApi";
 import { Enemy } from "../types";
+import { updateUserStat } from "../api/userApi";
 
 const QuestPage: React.FC = () => {
-  const { user } = useAuth();
   const [enemy, setEnemy] = useState<Enemy | null>(null);
-  const navigate = useNavigate();
+  const { user, session } = useAuth();
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
     handleGetEnemy();
-    // console.log(enemy);
-  }, []);
+  }, [user]);
 
-  const handleGetEnemy = async () => {
-    const newEnemy = await fetchEnemyById("1");
-    setEnemy(newEnemy);
-    console.log(user);
-    console.log(enemy);
+  // Show message in the modal for 3 seconds
+  const showMessage = (msg: string) => {
+    setMessage(msg);
+    setTimeout(() => {
+      setMessage("");
+      window.location.reload();
+    }, 3000);
   };
 
-  const handleFight = () => {
+  const handleGetEnemy = async () => {
+    if (user) {
+      const newEnemyId = user.enemies_defeated + 1;
+      const newEnemyIdToString = newEnemyId.toString();
+      const newEnemy = await fetchEnemyById(newEnemyIdToString);
+      setEnemy(newEnemy);
+    }
+  };
+
+  const handleFightLossMessage = () => {
+    let lowStats = "";
+    if (
+      enemy?.calorie_exp &&
+      user &&
+      user.pet_calorie_exp < enemy?.calorie_exp
+    ) {
+      lowStats += " calorie";
+    }
+    if (enemy?.carb_exp && user && user.pet_carb_exp < enemy?.carb_exp) {
+      lowStats += "  carb";
+    }
+    if (enemy?.fat_exp && user && user.pet_fat_exp < enemy?.fat_exp) {
+      lowStats += " fat";
+    }
+    if (
+      enemy?.protein_exp &&
+      user &&
+      user.pet_protein_exp < enemy?.protein_exp
+    ) {
+      lowStats += " protein";
+    }
+    if (enemy?.wisdom_exp && user && user.pet_wisdom_exp < enemy?.wisdom_exp) {
+      lowStats += " wisdom";
+    }
+    return lowStats;
+  };
+
+  const handleFight = async () => {
     if (
       user.pet_calorie_exp >= enemy?.calorie_exp &&
       user.pet_carb_exp >= enemy?.carb_exp &&
@@ -31,9 +69,18 @@ const QuestPage: React.FC = () => {
       user.pet_protein_exp >= enemy?.protein_exp &&
       user.pet_wisdom_exp >= enemy?.wisdom_exp
     ) {
-      console.log("you win!");
+      //   console.log("you win!");
+      await updateUserStat(
+        user?.id,
+        user?.enemies_defeated + 1,
+        "enemies_defeated",
+        session?.access_token
+      );
+      showMessage("You won!");
     } else {
-      console.log("you lose :(");
+      showMessage(
+        `You lost! The following stats are too low:${handleFightLossMessage()}`
+      );
     }
   };
 
@@ -41,16 +88,21 @@ const QuestPage: React.FC = () => {
     <div className="min-h-screen flex flex-col items-center justify-center pb-16">
       <div className="bg-[url('/paper-box.jpg')] bg-repeat w-5/6 h-[80vh] flex justify-around p-4">
         <div className="flex flex-col items-center">
-          <h1 className="text-4xl">{enemy?.name}</h1>
+          <h1 className="font-bold text-4xl m-1 p-1">Current Quest</h1>
+          <h1 className="text-4xl m-1 p-1">Defeat a {enemy?.name}!</h1>
           <img src={enemy?.img} alt="" />
           <button
-            onClick={() => {
-              handleFight();
-            }}
-            className="m-1 p-1 w-1/4 bg-blue-500"
+            onClick={handleFight}
+            className="m-1 p-1 w-1/4 text-2xl bg-blue-500"
           >
             Fight
           </button>
+          {/* Display confirmation or error message */}
+          {message && (
+            <div className="absolute bottom-20 left-1/2 -translate-x-1/2 -translate-y-2/3 z-10 rounded-sm px-5 py-2 bg-black opacity-70 min-w-3xs lg:top-3/4 lg:-translate-y-4/5">
+              <p className="text-center text-white">{message}</p>{" "}
+            </div>
+          )}
         </div>
       </div>
     </div>
