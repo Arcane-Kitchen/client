26224d6b232812771133dbc14ea959f3d84522db
Karen Saki Miyazawa <ramos.karensaki@gmail.com>
2025-03-24 13:41:15 +0900

    fix: refactor handleupdateStat to include all stats in a single call

--- src/components/RecipeModal.tsx
+++ src/components/RecipeModal.tsx
@@ -14,7 +14,6 @@ import {
 } from "../api/mealPlanApi";
 import moment from "moment";
 import { updateUserPetStat } from "../api/userApi";
-import { fetchARecipeById } from "../api/recipeApi";
 import { addUserActivity } from "../api/activityApi";
 import { handlePointCalc, handleRatioCalc } from "../util/statCalc";
 import { dietColors } from "../util/constants"
@@ -71,72 +70,30 @@ const RecipeModal: React.FC<ModalProps> = ({
   };
 
   // Handle updating stat
-  const handleUpdateStat = async (
-    statName: string,
-    eatenMacroValue: number,
-    isSubtraction: boolean
-  ) => {
-    switch (statName) {
-      case "carb":
-        if (user?.daily_carb_goal) {
-          const ratio = handleRatioCalc(user.daily_carb_goal, eatenMacroValue);
-          const points = handlePointCalc(ratio, isSubtraction);
-          await updateUserPetStat(
-            user.id,
-            user.pet_carb_exp + points,
-            "carb",
-            session!.access_token
-          );
-          return points;
-        }
-        break;
-      case "fat":
-        if (user?.daily_fat_goal) {
-          const ratio = handleRatioCalc(user.daily_fat_goal, eatenMacroValue);
-          const points = handlePointCalc(ratio, isSubtraction);
-          await updateUserPetStat(
-            user.id,
-            user.pet_fat_exp + points,
-            "fat",
-            session!.access_token
-          );
-          return points;
-        }
-        break;
-      case "protein":
-        if (user?.daily_protein_goal) {
-          const ratio = handleRatioCalc(
-            user.daily_protein_goal,
-            eatenMacroValue
-          );
-          const points = handlePointCalc(ratio, isSubtraction);
-          await updateUserPetStat(
-            user.id,
-            user.pet_protein_exp + points,
-            "protein",
-            session!.access_token
-          );
-          return points;
-        }
-        break;
-      case "calorie":
-        if (user?.daily_calorie_goal) {
-          const ratio = handleRatioCalc(
-            user.daily_calorie_goal / 3,
-            eatenMacroValue
-          );
-          const points = handlePointCalc(ratio, isSubtraction);
-          await updateUserPetStat(
-            user.id,
-            user.pet_calorie_exp + points,
-            "calorie",
-            session!.access_token
-          );
-          return points;
-        }
-        break;
+  const handleUpdateAllStats = async (allStats: {calorie: number, carb: number, fat: number, protein: number}, isSubtraction:boolean) => {
+    if (user && session) {
+      const carbRatio = handleRatioCalc(user.daily_carb_goal, allStats.carb);
+      const carbPoints = handlePointCalc(carbRatio, isSubtraction);
+
+      const fatRatio = handleRatioCalc(user.daily_fat_goal, allStats.fat);
+      const fatPoints = handlePointCalc(fatRatio, isSubtraction);
+
+      const proteinRatio = handleRatioCalc(user.daily_protein_goal, allStats.protein);
+      const proteinPoints = handlePointCalc(proteinRatio, isSubtraction);
+
+      const calorieRatio = handleRatioCalc(user.daily_calorie_goal / 3, allStats.calorie);
+      const caloriePoints = handlePointCalc(calorieRatio, isSubtraction);
+
+      await updateUserPetStat(user.id, { 
+        carb: user.pet_carb_exp  + carbPoints,
+        fat: user.pet_fat_exp + fatPoints,
+        protein: user.pet_protein_exp + proteinPoints,
+        calorie: user.pet_calorie_exp + caloriePoints
+      }, session.access_token)
+
+      return { carb: carbPoints, fat: fatPoints,  protein: proteinPoints, calorie: caloriePoints}
     }
-  };
+  }
 
   // Add recipe to the meal plan
   const handleAddClick = async () => {
@@ -198,12 +155,13 @@ const RecipeModal: React.FC<ModalProps> = ({
         if (newMeal) {
           await updateUserPetStat(
             user.id,
-            user.pet_wisdom_exp + 20,
-            "wisdom",
+            { wisdom: user.pet_wisdom_exp + 20 },
             session.access_token
           );
+
           showMessage(`Recipe added to meal plan: Wisdom +20`);
           setMealPlan([...mealPlan, newMeal.mealPlan]);
+
           // update current page user wisdom exp so that a user can add muliple recipes in a row and get all the wisdom points
           const updatedUser = { ...user };
           updatedUser.pet_wisdom_exp = updatedUser.pet_wisdom_exp + 20;
@@ -257,36 +215,21 @@ const RecipeModal: React.FC<ModalProps> = ({
           // if meal was uneaten (this block), subtract strength, defense, dex, and stamina points (selectedMeal.hasBeenEaten is reversed)
           if (selectedMeal.hasBeenEaten) {
             // first, grab eaten meal's nutrition info
-            const recipe = await fetchARecipeById(meal.recipeId);
-            const nutrition = recipe.nutrition;
-            const eatenCalories = nutrition.calories;
-            const eatenCarbPercent = nutrition.macronutrients.carbs.percentage;
-            const eatenFatPercent = nutrition.macronutrients.fat.percentage;
-            const eatenProteinPercent =
-            nutrition.macronutrients.protein.percentage;
+            const eatenCalories = meal.calories;
+            const eatenCarbPercent = meal.macronutrients.carbs;
+            const eatenFatPercent = meal.macronutrients.fat;
+            const eatenProteinPercent = meal.macronutrients.protein;
+            
             // next, calculate points for each stat and update
-            const caloriePoints = await handleUpdateStat(
-              "calorie",
-              eatenCalories,
-              true
-            );
-            const carbPoints = await handleUpdateStat(
-              "carb",
-              eatenCarbPercent,
-              true
-            );
-            const fatPoints = await handleUpdateStat(
-              "fat",
-              eatenFatPercent,
-              true
-            );
-            const proteinPoints = await handleUpdateStat(
-              "protein",
-              eatenProteinPercent,
-              true
-            );
+            const allPoints = await handleUpdateAllStats({ 
+              calorie: eatenCalories,
+              carb: eatenCarbPercent,
+              fat: eatenFatPercent,
+              protein: eatenProteinPercent
+            }, true)
+            
             showMessage(
-              `Strength ${proteinPoints} Defense ${fatPoints} Dexterity ${carbPoints} Stamina ${caloriePoints}`
+              `Strength ${allPoints?.protein} Defense ${allPoints?.fat} Dexterity ${allPoints?.carb} Stamina ${allPoints?.calorie}`
             );
 
             setTimeout(() => {
@@ -294,67 +237,44 @@ const RecipeModal: React.FC<ModalProps> = ({
               setMealPlan(updatedMealPlan);
               // update current page user data to make sure eating multiple things in a row gives all the points
               const updatedUser = { ...user };
-              if (proteinPoints)
-                updatedUser.pet_protein_exp =
-                  updatedUser.pet_protein_exp + proteinPoints;
-              if (fatPoints)
-                updatedUser.pet_fat_exp = updatedUser.pet_fat_exp + fatPoints;
-              if (carbPoints)
-                updatedUser.pet_carb_exp = updatedUser.pet_carb_exp + carbPoints;
-              if (caloriePoints)
-                updatedUser.pet_calorie_exp =
-                  updatedUser.pet_calorie_exp + caloriePoints;
+              if (allPoints) {
+                updatedUser.pet_protein_exp += allPoints.protein;
+                updatedUser.pet_fat_exp += allPoints.fat;
+                updatedUser.pet_carb_exp += allPoints.carb;
+                updatedUser.pet_calorie_exp += allPoints.calorie;
+              }
               setUser(updatedUser);
               onClose();
             }, 3000);
           } else if (!selectedMeal.hasBeenEaten) {
             // if meal was eaten (this block), add stats
             // first, grab eaten meal's nutrition info
-            const recipe = await fetchARecipeById(meal.recipeId);
-            const nutrition = recipe.nutrition;
-            const eatenCalories = nutrition.calories;
-            const eatenCarbPercent = nutrition.macronutrients.carbs.percentage;
-            const eatenFatPercent = nutrition.macronutrients.fat.percentage;
-            const eatenProteinPercent =
-              nutrition.macronutrients.protein.percentage;
+            const eatenCalories = meal.calories;
+            const eatenCarbPercent = meal.macronutrients.carbs;
+            const eatenFatPercent = meal.macronutrients.fat;
+            const eatenProteinPercent = meal.macronutrients.protein;
+
             // next, calculate points for each stat and update
-            const caloriePoints = await handleUpdateStat(
-              "calorie",
-              eatenCalories,
-              false
-            );
-            const carbPoints = await handleUpdateStat(
-              "carb",
-              eatenCarbPercent,
-              false
-            );
-            const fatPoints = await handleUpdateStat(
-              "fat",
-              eatenFatPercent,
-              false
-            );
-            const proteinPoints = await handleUpdateStat(
-              "protein",
-              eatenProteinPercent,
-              false
-            );
+            const allPoints = await handleUpdateAllStats({ 
+              calorie: eatenCalories,
+              carb: eatenCarbPercent,
+              fat: eatenFatPercent,
+              protein: eatenProteinPercent
+            }, false)
+
             showMessage(
-              `Strength +${proteinPoints} Defense +${fatPoints} Dexterity +${carbPoints} Stamina +${caloriePoints}`
+              `Strength +${allPoints?.protein} Defense +${allPoints?.fat} Dexterity +${allPoints?.carb} Stamina +${allPoints?.calorie}`
             );
 
             setTimeout(() => {
               // update current page user data to make sure eating multiple things in a row gives all the points
               const updatedUser = { ...user };
-              if (proteinPoints)
-                updatedUser.pet_protein_exp =
-                  updatedUser.pet_protein_exp + proteinPoints;
-              if (fatPoints)
-                updatedUser.pet_fat_exp = updatedUser.pet_fat_exp + fatPoints;
-              if (carbPoints)
-                updatedUser.pet_carb_exp = updatedUser.pet_carb_exp + carbPoints;
-              if (caloriePoints)
-                updatedUser.pet_calorie_exp =
-                  updatedUser.pet_calorie_exp + caloriePoints;
+              if (allPoints) {
+                updatedUser.pet_protein_exp += allPoints.protein;
+                updatedUser.pet_fat_exp += allPoints.fat;
+                updatedUser.pet_carb_exp += allPoints.carb;
+                updatedUser.pet_calorie_exp += allPoints.calorie;
+              }
               setUser(updatedUser);
               onClose();
             }, 3000);
@@ -407,9 +327,7 @@ const RecipeModal: React.FC<ModalProps> = ({
       );
       // subtract 20 points of wisdom if removing meal from plan
       await updateUserPetStat(
-        user.id,
-        user.pet_wisdom_exp - 20,
-        "wisdom",
+        user.id, { wisdom: user.pet_wisdom_exp - 20 },
         session.access_token
       );
       showMessage("Wisdom -20");
